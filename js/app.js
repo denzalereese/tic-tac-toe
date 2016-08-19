1378899ac1f04b280321510b05e1c3edbef4c581
Denzale Reese <dlr249@cornell.edu>
2016-08-19 16:15:19 -0700

    adds game board, click functionality for X/O's, and detects X wins in the top row only

--- js/app.js
+++ js/app.js
@@ -2,5 +2,31 @@
 $(document).ready(function() {
   // all code to manipulate the DOM
   // goes inside this function
+  var clicks = 1;
+  $(".box").on("click", function() {
+    console.log(this.id);
+     if ($("#" + this.id).is(":empty")) {
+      clicks ++;
+      if (clicks % 2 == 0) {
+        $("#" + this.id).addClass("clicked-X");
+        $("#" + this.id).append("<h1>X</h1>");
+      }else{
+        $("#" + this.id).addClass("clicked-O");
+        $("#" + this.id).append("<h1>O</h1>");
+      }
 
+      if (checkWin()) {
+        console.log("Win!!!!");
+      }
+   }
+  });
+
+  function checkWin() {
+    if ($("#top-left").hasClass("clicked-X") &&
+       $("#top-mid").hasClass("clicked-X") &&
+       $("#top-right").hasClass("clicked-X")) {
+      return true;
+    }
+      return false;
+    }
 });
