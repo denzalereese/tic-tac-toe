97b653cf5892358e0ec77020d864531da140e3de
Denzale Reese <dlr249@cornell.edu>
2016-08-19 16:35:58 -0700

    adds all ways to win for both X and O, changes the header when the respective user wins

--- js/app.js
+++ js/app.js
@@ -15,18 +15,129 @@ $(document).ready(function() {
         $("#" + this.id).append("<h1>O</h1>");
       }
 
-      if (checkWin()) {
-        console.log("Win!!!!");
+      if (checkXWin()) {
+        $("#header-text").text("X wins!!");
+      }
+      else if (checkOWin()){
+        $("#header-text").text("O wins!!");
       }
    }
   });
 
-  function checkWin() {
+  function checkXWin() {
+    //horizontal wins
     if ($("#top-left").hasClass("clicked-X") &&
        $("#top-mid").hasClass("clicked-X") &&
        $("#top-right").hasClass("clicked-X")) {
       return true;
     }
+
+    else if ($("#mid-left").hasClass("clicked-X") &&
+       $("#mid-mid").hasClass("clicked-X") &&
+       $("#mid-right").hasClass("clicked-X")) {
+      return true;
+    }
+
+    else if ($("#bottom-left").hasClass("clicked-X") &&
+       $("#bottom-mid").hasClass("clicked-X") &&
+       $("#bottom-right").hasClass("clicked-X")) {
+      return true;
+    }
+
+    //vertical wins
+
+    else if ($("#top-left").hasClass("clicked-X") &&
+       $("#mid-left").hasClass("clicked-X") &&
+       $("#bottom-left").hasClass("clicked-X")) {
+      return true;
+    }
+
+    else if ($("#top-mid").hasClass("clicked-X") &&
+       $("#mid-mid").hasClass("clicked-X") &&
+       $("#bottom-mid").hasClass("clicked-X")) {
+      return true;
+    }
+
+    else if ($("#top-right").hasClass("clicked-X") &&
+       $("#mid-right").hasClass("clicked-X") &&
+       $("#bottom-right").hasClass("clicked-X")) {
+      return true;
+    }
+
+    //diagnol wins
+
+    else if ($("#top-left").hasClass("clicked-X") &&
+       $("#mid-mid").hasClass("clicked-X") &&
+       $("#bottom-right").hasClass("clicked-X")) {
+      return true;
+    }
+
+    else if ($("#top-right").hasClass("clicked-X") &&
+       $("#mid-mid").hasClass("clicked-X") &&
+       $("#bottom-left").hasClass("clicked-X")) {
+      return true;
+    }
+
       return false;
     }
+
+
+    function checkOWin() {
+    //horizontal wins
+    if ($("#top-left").hasClass("clicked-O") &&
+       $("#top-mid").hasClass("clicked-O") &&
+       $("#top-right").hasClass("clicked-O")) {
+      return true;
+    }
+
+    else if ($("#mid-left").hasClass("clicked-O") &&
+       $("#mid-mid").hasClass("clicked-O") &&
+       $("#mid-right").hasClass("clicked-O")) {
+      return true;
+    }
+
+    else if ($("#bottom-left").hasClass("clicked-O") &&
+       $("#bottom-mid").hasClass("clicked-O") &&
+       $("#bottom-right").hasClass("clicked-O")) {
+      return true;
+    }
+
+    //vertical wins
+
+    else if ($("#top-left").hasClass("clicked-O") &&
+       $("#mid-left").hasClass("clicked-O") &&
+       $("#bottom-left").hasClass("clicked-O")) {
+      return true;
+    }
+
+    else if ($("#top-mid").hasClass("clicked-O") &&
+       $("#mid-mid").hasClass("clicked-O") &&
+       $("#bottom-mid").hasClass("clicked-O")) {
+      return true;
+    }
+
+    else if ($("#top-right").hasClass("clicked-O") &&
+       $("#mid-right").hasClass("clicked-O") &&
+       $("#bottom-right").hasClass("clicked-O")) {
+      return true;
+    }
+
+    //diagnol wins
+
+    else if ($("#top-left").hasClass("clicked-O") &&
+       $("#mid-mid").hasClass("clicked-O") &&
+       $("#bottom-right").hasClass("clicked-O")) {
+      return true;
+    }
+
+    else if ($("#top-right").hasClass("clicked-O") &&
+       $("#mid-mid").hasClass("clicked-O") &&
+       $("#bottom-left").hasClass("clicked-O")) {
+      return true;
+    }
+
+      return false;
+    }
+
+
 });
